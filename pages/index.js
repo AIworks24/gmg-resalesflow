6d742b2401f89a3f5b58877959267b3542ea627c
AIworks24 <matt@aiworksconsulting.com>
2025-06-02 20:45:52 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,9 +52,9 @@
             }}
             className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             placeholder="License #"
-       />
+     />
       </div>
-    )}
+    </div>
   </div>
 );
 
